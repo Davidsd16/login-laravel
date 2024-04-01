ab5dcdc63ba4332a58e348678c7651d6dd02cf49
david <davidsanchezdiaz72@gmail.com>
2024-04-01 10:02:37 +0200

    Añadir boostrap a templates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
+@extends('layout.app-master')
+
+@section('content')
+
     <h1>Home</h1>
 
     @auth
@@ -19,5 +14,6 @@
     @guest
         <p> Bienvenido, para ver el contenido <a href="\login">inicia sesion</p>
     @endguest
-</body>
-</html>
+    
+@endsection
+
