92a8db85319db5a74cc91f464f5c12cc6b5495c9
david <davidsanchezdiaz72@gmail.com>
2024-04-01 08:52:41 +0200

    Autentificacion en templates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <h1>Home</h1>
+
+    @auth
+        <p>Bienvenido {{ auth()->user()->name ?? auth()->user()->username }} estás autentificado en esta página</p>
+    @endauth
+
+    @guest
+        <p> Bienvenido, para ver el contenido <a href="\login">inicia sesion</p>
+    @endguest
 </body>
 </html>
