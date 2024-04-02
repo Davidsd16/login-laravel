4e16e5794affbe9e5536744a8b210c29506147b5
david <davidsanchezdiaz72@gmail.com>
2024-04-02 19:58:53 +0200

    Mejora der vista register.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,19 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
-</head>
-<body>
+@extends('layouts.auth-master')
+
+@section('content')
     <form action="/register" method="POST">
         @csrf
-        <input type="text" name="username" placeholder="username">
-        <input type="email" name="email" placeholder="email">
-        <input type="password" name="password" placeholder="password">
-        <input type="password" name="password_confirmation" placeholder="password_confirmation">
-        <input type="submit" value="Registrarse">
+        <div class="form-floating mb-3">
+            <input type="email" name="email" placeholder="Username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Username</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" name="password_confirmation" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Password Confirmation</label>
+        </div>
+        <div class="mb-3">
+            <a href="/login">Login</a>
+        </div>
+        <button type="submit" class="btn btn-primary">Create Acoount</button>
     </form>
-</body>
-</html>
+
+@endsection
